40d85096952464ab0ffd5d8828a5dde75d97e2cc
vardhan2027 <itzvardhan2027@gmail.com>
2025-12-10 16:57:13 +0530

    Add files via upload

--- wroom_now_rec.ino
+++ wroom_now_rec.ino
@@ -11,6 +11,38 @@ const int IN2 = 25;
 const int IN3 = 33;
 const int IN4 = 32;
 
+
+#define THUMB_PIN   34   // FSR Thumb
+#define INDEX_PIN   35   // FSR Index
+#define MIDDLE_PIN  13   // FSR Middle
+
+// ---- Setpoints (from your calibration) ----
+const float SP_THUMB  = 825.72;
+const float SP_INDEX  = 323.50;
+const float SP_MIDDLE = 690.96;
+
+// Read one FSR: ignore first 2 samples, average next 5
+float readFSRavg(int pin) {
+  int raw;
+
+  // discard first 2 readings
+  for (int i = 0; i < 2; i++) {
+    raw = analogRead(pin);
+    delay(50);
+  }
+
+  // take next 5 readings for average
+  long sum = 0;
+  for (int i = 0; i < 5; i++) {
+    raw = analogRead(pin);
+    sum += raw;
+    delay(5);
+  }
+
+  float avg = sum / 5.0;
+  return avg;
+}
+
 typedef struct {
   uint8_t command; // 0=stop, 1=extend, 2=retract
 } rx_message_t;
@@ -84,6 +116,7 @@ void setup() {
   Serial.begin(9600);
   WiFi.mode(WIFI_STA);
   delay(100);
+  Serial.println("ESP32 – 3 FSR threshold test started...");
   Serial.print("My MAC: ");
   Serial.println(WiFi.macAddress());
   pinMode(IN1, OUTPUT);
@@ -110,5 +143,29 @@ void setup() {
 
 void loop() {
   // nothing here — all handled in callback
+    // 1️⃣ Get averaged readings for each finger
+  float thumbAvg  = readFSRavg(THUMB_PIN);
+  float indexAvg  = readFSRavg(INDEX_PIN);
+  float middleAvg = readFSRavg(MIDDLE_PIN);
+
+  // 2️⃣ Print averages
+  Serial.println("========== FSR AVERAGES ==========");
+  Serial.print("Thumb  avg : ");  Serial.println(thumbAvg);
+  Serial.print("Index  avg : ");  Serial.println(indexAvg);
+  Serial.print("Middle avg : ");  Serial.println(middleAvg);
+
+  // 3️⃣ Check AND condition for all three fingers
+  bool conditionOK =
+      (thumbAvg -100 >= SP_THUMB)  ||
+      (indexAvg +35  >= SP_INDEX)  ||
+      (middleAvg - 3000>= SP_MIDDLE);
+
+
+  if(conditionOK){
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  }
   delay(100);
 }
